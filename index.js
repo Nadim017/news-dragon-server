0b3d0b707fbcc62dcbf0c1ccbf273dd0bb699489
nadim <mdnadimmahamud704@gmail.com>
2023-04-20 16:32:47 +0600

    server path updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.get('/', (req, res) => {
   res.send('hello from the other side after updated nodemon');
 });
-app.get('/category', (req, res) => {
+app.get('/categories', (req, res) => {
   res.send(categories);
 });
 app.listen(port, () => {
